b08ea499e0b8f1084fa6e0127ff2f43063ceb063
Jeevitha A <jeevithaanand918@gmail.com>
2026-07-23 12:12:37 +0530

    Task10.js

--- Set1/Task10.js
+++ Set1/Task10.js
@@ -1,9 +1,3 @@
-/* ============================================================
-   Task 10: Student Record System — Create and Print an Object
-   → Create a `student` object with at least 5 properties and
-     print each property individually.
-   ============================================================ */
-
 let student = {
     id: 101,
     name: "Ishaan",
@@ -18,11 +12,4 @@ console.log("Marks:", student.marks);
 console.log("Course:", student.course);
 console.log("Semester:", student.semester);
 
-/* ------------------------------------------------------------
-   Expected Sample Output:
-   ID: 101
-   Name: Ishaan
-   Marks: 88
-   Course: Node & Express JS
-   Semester: 5
-   ------------------------------------------------------------ */
+
