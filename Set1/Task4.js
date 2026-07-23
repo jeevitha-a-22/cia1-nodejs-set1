3a66ffa686b1b9d91971613ac1c136de336b33a6
Jeevitha A <jeevithaanand918@gmail.com>
2026-07-23 12:15:16 +0530

    Update Task4.js

--- Set1/Task4.js
+++ Set1/Task4.js
@@ -1,18 +1,6 @@
-/* ============================================================
-   Task 4: Student Record System — Perform a Calculation
-   → Write a program to calculate the average marks of 3
-     students using arithmetic operators, and print the result.
-   ============================================================ */
-
 let marks1 = 78;
 let marks2 = 88;
 let marks3 = 92;
-
 let averageMarks = (marks1 + marks2 + marks3) / 3;
-
 console.log("Average Marks of 3 students:", averageMarks);
 
-/* ------------------------------------------------------------
-   Expected Sample Output:
-   Average Marks of 3 students: 86
-   ------------------------------------------------------------ */
