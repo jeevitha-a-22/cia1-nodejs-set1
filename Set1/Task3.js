08b14e94d56473a19f06f30d53db446ed974b2ee
Jeevitha A <jeevithaanand918@gmail.com>
2026-07-23 12:14:57 +0530

    Update Task3.js

--- Set1/Task3.js
+++ Set1/Task3.js
@@ -1,20 +1,8 @@
-/* ============================================================
-   Task 3: Student Record System — Identify Datatypes
-   → Declare variables of type Number, String, and Boolean
-     related to a student, and print their datatypes using typeof.
-   ============================================================ */
-
-let studentMarks = 85;          // Number
-let studentName = "Ananya";     // String
-let isPassed = true;            // Boolean
+let studentMarks = 85;          
+let studentName = "Ananya";     
+let isPassed = true;            
 
 console.log("studentMarks:", studentMarks, "-> typeof:", typeof studentMarks);
 console.log("studentName:", studentName, "-> typeof:", typeof studentName);
 console.log("isPassed:", isPassed, "-> typeof:", typeof isPassed);
 
-/* ------------------------------------------------------------
-   Expected Sample Output:
-   studentMarks: 85 -> typeof: number
-   studentName: Ananya -> typeof: string
-   isPassed: true -> typeof: boolean
-   ------------------------------------------------------------ */
